7ab4eaeda21c1811dca22e70ca1e8cf817eb195b
tcharuel <16272579+ThomasCharuel@users.noreply.github.com>
2024-02-01 15:23:36 +0100

    update

--- src/main.c
+++ src/main.c
@@ -6,16 +6,16 @@
 /*   By: tcharuel <tcharuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 10:08:12 by tcharuel          #+#    #+#             */
-/*   Updated: 2024/02/01 11:55:58 by tcharuel         ###   ########.fr       */
+/*   Updated: 2024/02/01 14:43:54 by tcharuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 // Ctrl + C
-void	sigInt(int code)
+void	sigint_handler(int signum)
 {
-	(void)code;
+	(void)signum;
 	ft_printf("\n");
 	rl_on_new_line();
 	rl_replace_line("", 0);
@@ -23,18 +23,16 @@ void	sigInt(int code)
 }
 
 // Ctrl + D
-void	sigTerm(int code)
+void	sigterm_handler(int signum)
 {
-	(void)code;
-	ft_printf("\nexit\n");
+	(void)signum;
 	exit(EXIT_SUCCESS);
 }
 
 // Ctrl + backslash
-void	sigQuit(int code)
+void	sigquit_handler(int signum)
 {
-	(void)code;
-	printf("yo\n");
+	(void)signum;
 }
 
 int	main(int argc, char **argv)
@@ -43,19 +41,19 @@ int	main(int argc, char **argv)
 
 	(void)argc;
 	(void)argv;
-	signal(SIGTERM, &sigTerm);
-	signal(SIGINT, &sigInt);
-	signal(SIGQUIT, &sigQuit);
-	line = prompt_loop();
-	while (line)
+	signal(SIGTERM, &sigterm_handler);
+	signal(SIGINT, &sigint_handler);
+	signal(SIGQUIT, &sigquit_handler);
+	while (true)
 	{
+		line = prompt_loop();
 		if (!is_whitespace_line(line))
 		{
 			add_history(line);
 			ft_printf("%s\n", line);
 		}
 		free(line);
-		line = prompt_loop();
 	}
+	ft_printf("\nexit\n");
 	return (0);
 }
