7f4b96e5f1bc8314dce8eb79e25414b46535a4c7
tcharuel <16272579+ThomasCharuel@users.noreply.github.com>
2024-02-03 14:22:12 +0100

    BEfore going from dict to strs

--- src/env.c
+++ src/env.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   state.c                                            :+:      :+:    :+:   */
+/*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tcharuel <tcharuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/03 11:45:39 by tcharuel          #+#    #+#             */
-/*   Updated: 2024/02/03 14:15:44 by tcharuel         ###   ########.fr       */
+/*   Created: 2024/02/03 14:13:29 by tcharuel          #+#    #+#             */
+/*   Updated: 2024/02/03 14:22:08 by tcharuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_dict	*init_vars_dict(char **envp)
+t_dict	*env_to_dict(char **envp)
 {
 	t_dict	*vars;
 	size_t	i;
@@ -36,26 +36,6 @@ t_dict	*init_vars_dict(char **envp)
 	return (vars);
 }
 
-t_state	*state_init(char **envp)
+char	**dict_to_env(t_dict *dict)
 {
-	t_state	*state;
-
-	state = (t_state *)malloc(sizeof(t_state));
-	if (!state)
-		return (NULL);
-	state->vars = env_to_dict(envp);
-	if (!state->vars)
-		return (free(state), state = NULL, NULL);
-	return (state);
-}
-
-void	state_cleanup(t_state *state)
-{
-	if (state)
-	{
-		if (state->vars)
-			ft_dict_destroy(&state->vars);
-		free(state);
-		state = NULL;
-	}
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tcharuel <tcharuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 10:08:12 by tcharuel          #+#    #+#             */
-/*   Updated: 2024/02/03 12:03:45 by tcharuel         ###   ########.fr       */
+/*   Updated: 2024/02/03 14:17:02 by tcharuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,51 +14,55 @@
 
 volatile sig_atomic_t	g_signal_code;
 
-// void	ft_input_redirect(char *in_file_path)
-// {
-// 	int	in;
+void	ft_input_redirect(char *in_file_path)
+{
+	int	in;
 
-// 	in = open(in_file_path, O_RDONLY);
-// 	if (dup2(in, STDIN_FILENO) == -1)
-// 	{
-// 		close(in);
-// 		exit(EXIT_FAILURE);
-// 	}
-// 	close(in);
-// }
+	in = open(in_file_path, O_RDONLY);
+	if (dup2(in, STDIN_FILENO) == -1)
+	{
+		close(in);
+		exit(EXIT_FAILURE);
+	}
+	close(in);
+}
 
-// struct dirent	*read_dir(char **path, char *cmd)
-// {
-// 	struct dirent	*file;
-// 	DIR				*dir;
-// 	int				i;
+struct dirent	*read_dir(char **path, char *cmd)
+{
+	struct dirent	*file;
+	DIR				*dir;
+	int				i;
 
-// 	i = 0;
-// 	dir = opendir(path[i++]);
-// 	while (dir)
-// 	{
-// 		file = readdir(dir);
-// 		while (file)
-// 		{
-// 			if (!ft_strncmp(file->d_name, cmd, ft_strlen(file->d_name)))
-// 				return (file);
-// 			file = readdir(dir);
-// 		}
-// 		dir = opendir(path[i++]);
-// 	}
-// 	return (NULL);
-// }
+	i = 0;
+	dir = opendir(path[i++]);
+	while (dir)
+	{
+		file = readdir(dir);
+		while (file)
+		{
+			if (!ft_strncmp(file->d_name, cmd, ft_strlen(file->d_name)))
+				return (file);
+			file = readdir(dir);
+		}
+		dir = opendir(path[i++]);
+	}
+	return (NULL);
+}
 
-// void	ft_exec(char *command, char **envp)
-// {
-// 	char			*argv[] = {command, NULL};
-// 	char			**path;
-// 	struct dirent	*file;
+t_return_code	ft_exec(t_state *state, char *command)
+{
+	char			*argv[] = {command, NULL};
+	char			**path;
+	struct dirent	*file;
 
-// 	path = ft_split(get_var(data, "PATH")->value, ':');
-// 	file = read_dir(path, command);
-// 	execve(file->d_name, argv, envp);
-// }
+	path = ft_split(ft_dict_get(state->vars, "PATH"), ':');
+	if (!path)
+		return (ERROR);
+	file = read_dir(path, command);
+	if (fork())
+		execve(file->d_name, argv, envp);
+	return (SUCCESS);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -79,8 +83,11 @@ int	main(int argc, char **argv, char **envp)
 		if (!is_whitespace_line(line))
 		{
 			add_history(line);
-			ft_printf(line);
-			// ft_exec(line, envp);
+			if (!ft_exec(state, line))
+			{
+				free(line);
+				break ;
+			}
 		}
 		free(line);
 	}
