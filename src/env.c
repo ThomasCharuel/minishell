416f4231ee894e8ae7e013f9c3b189e6afc31c11
tcharuel <16272579+ThomasCharuel@users.noreply.github.com>
2024-02-13 18:02:42 +0100

    export no arg

--- src/env.c
+++ src/env.c
@@ -6,7 +6,7 @@
 /*   By: tcharuel <tcharuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 17:17:41 by tcharuel          #+#    #+#             */
-/*   Updated: 2024/02/13 17:35:39 by tcharuel         ###   ########.fr       */
+/*   Updated: 2024/02/13 18:02:35 by tcharuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_command_status	minishell_env(t_state *state, int argc, char **argv)
 {
 	size_t	i;
 
-	(void)argc;
+	// N'affiche pas truc vide
 	(void)argv;
 	if (argc > 1)
 		return (print_error("env: too many arguments", NULL),
@@ -27,11 +27,42 @@ t_command_status	minishell_env(t_state *state, int argc, char **argv)
 	return (COMMAND_SUCCESS);
 }
 
+t_command_status	print_declare_statements(char **envp)
+{
+	size_t	i;
+	char	*declare_statement;
+	char	**strs;
+
+	i = 0;
+	while (envp[i])
+	{
+		strs = ft_split(envp[i], '=');
+		if (!strs)
+			return (COMMAND_ERROR);
+		if (strs[1])
+			declare_statement = ft_strsjoin("declare -x ", strs[0], "=\"",
+					strs[1], "\"", NULL);
+		else
+			declare_statement = ft_strsjoin("declare -x ", strs[0], NULL);
+		ft_clean_double_list((void **)strs, free);
+		if (!declare_statement)
+			return (COMMAND_ERROR);
+		ft_printf("%s\n", declare_statement);
+		i++;
+	}
+	return (COMMAND_SUCCESS);
+}
+
 t_command_status	minishell_export(t_state *state, int argc, char **argv)
 {
-	(void)state;
-	(void)argc;
+	char	*key;
+	char	*value;
+
+	if (argc == 1)
+		return (print_declare_statements(state->envp));
 	(void)argv;
+	if (!envp_set(state, key, value))
+		return (COMMAND_ERROR);
 	return (COMMAND_SUCCESS);
 }
 
@@ -40,5 +71,6 @@ t_command_status	minishell_unset(t_state *state, int argc, char **argv)
 	(void)state;
 	(void)argc;
 	(void)argv;
+	// envp_delete(t_state *state, const char *key)
 	return (COMMAND_SUCCESS);
 }
