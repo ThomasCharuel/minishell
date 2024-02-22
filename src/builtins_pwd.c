c6fe9ffaf9677a0c4594cf19e1e063c786d1ee6c
tcharuel <16272579+ThomasCharuel@users.noreply.github.com>
2024-02-22 11:49:12 +0100

    Correct pwd options handling

--- src/builtins_pwd.c
+++ src/builtins_pwd.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pwd.c                                              :+:      :+:    :+:   */
+/*   builtins_pwd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rdupeux <rdupeux@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tcharuel <tcharuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 13:12:35 by rdupeux           #+#    #+#             */
-/*   Updated: 2024/02/20 13:54:45 by rdupeux          ###   ########.fr       */
+/*   Updated: 2024/02/22 11:49:00 by tcharuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,19 @@ t_command_status	minishell_pwd(t_state *state, int argc, char **argv)
 	char	*pwd;
 
 	(void)state;
-	(void)argc;
-	(void)argv;
+	if (argc > 1 && *argv[1] == '-' && argv[1][1])
+		return (print_error("pwd: ", argv[1], ": invalid option",
+				"\npwd: usage: pwd", NULL), COMMAND_PARSING_ERROR);
 	pwd = get_working_directory();
 	if (!pwd)
 		return (COMMAND_ERROR);
 	if (*pwd)
 		ft_printf("%s\n", pwd);
 	else
+	{
 		print_error("pwd: error: cannot determine current directory:",
 			"No such file or directory", NULL);
+	}
 	free(pwd);
 	return (COMMAND_SUCCESS);
 }
