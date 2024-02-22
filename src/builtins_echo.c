01598bc3088eec5a5ac7d462488720cba4abf1e6
tcharuel <16272579+ThomasCharuel@users.noreply.github.com>
2024-02-22 11:21:52 +0100

    Correct echo behavior

--- src/builtins_echo.c
+++ src/builtins_echo.c
@@ -6,11 +6,26 @@
 /*   By: tcharuel <tcharuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 13:10:57 by rdupeux           #+#    #+#             */
-/*   Updated: 2024/02/20 19:40:40 by tcharuel         ###   ########.fr       */
+/*   Updated: 2024/02/22 11:21:35 by tcharuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+// OK
+static bool	is_n_option(char *str)
+{
+	size_t	i;
+
+	if (*str != '-')
+		return (false);
+	i = 1;
+	while (str[i] == 'n')
+		i++;
+	if (!str[i])
+		return (true);
+	return (false);
+}
 
 t_command_status	minishell_echo(t_state *state, int argc, char **argv)
 {
@@ -20,7 +35,7 @@ t_command_status	minishell_echo(t_state *state, int argc, char **argv)
 	(void)state;
 	should_print_new_line = true;
 	i = 1;
-	while (argv[i] && !ft_strcmp("-n", argv[i]))
+	while (argv[i] && is_n_option(argv[i]))
 	{
 		should_print_new_line = false;
 		i++;
