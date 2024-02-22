e5a8adfd2598914b30d0303b6f384188cce63227
tcharuel <16272579+ThomasCharuel@users.noreply.github.com>
2024-02-22 12:00:41 +0100

    Comment

--- src/variable.c
+++ src/variable.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   variable.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rdupeux <rdupeux@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tcharuel <tcharuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 11:38:57 by tcharuel          #+#    #+#             */
-/*   Updated: 2024/02/20 18:22:35 by rdupeux          ###   ########.fr       */
+/*   Updated: 2024/02/22 11:55:41 by tcharuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,7 @@ t_command_status	handle_env_var(t_state *state, const char **ptr,
 
 	while (*ptr && **ptr)
 	{
+		// C'est faux ca, pourquoi get_next_word de espace
 		if (**ptr == '$' && ft_is_char_in_set(*(*ptr + 1),
 				"=_+*()[]{}|&\\,.;:!@#^\"\'"))
 			status = get_next_word(ptr, &word, " ", false);
